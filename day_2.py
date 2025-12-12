a3112c55a7d22c99bd3f34fa32480d7bc4052c6e
EsbernTK <esberntk@gmail.com>
2025-12-12 21:55:02 +0100

    completed day 2

--- day_2.py
+++ day_2.py
@@ -88,16 +88,7 @@ def find_duplicates_sum(data_list):
     return all_dupes, s
 
 
-
-
-
-if __name__ == '__main__':
-    test_data =["11-22","95-115","998-1012","1188511880-1188511890","222220-222224","1698522-1698528","446443-446449","38593856-38593862"]
-    expected_dupes = set([11, 22,99, 1010, 1188511885, 222222, 446446, 38593859])
-    dupes_test, s_tst = find_duplicates_sum(test_data)
-    #dupes_set = set(dupes_test)
-    assert s_tst == 1227775554
-
+def main_1():
     with open("day_2.txt") as f:
         data = f.read().strip().split(",")
 
@@ -107,6 +98,160 @@ if __name__ == '__main__':
 
     print(dupes)
     print(s) #19386344315
+
+def main_1_test():
+    test_data = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    test_data_list = test_data.strip().split(",")
+    expected_dupes = set([11, 22,99, 1010, 1188511885, 222222, 446446, 38593859])
+    dupes_test, s_tst = find_duplicates_sum(test_data_list)
+    #dupes_set = set(dupes_test)
+    assert s_tst == 1227775554
+
+"""
+--- Part Two ---
+The clerk quickly discovers that there are still invalid IDs in the ranges in your list. Maybe the young Elf was doing other silly patterns as well?
+
+Now, an ID is invalid if it is made only of some sequence of digits repeated at least twice. So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
+
+From the same example as before:
+
+11-22 still has two invalid IDs, 11 and 22.
+95-115 now has two invalid IDs, 99 and 111.
+998-1012 now has two invalid IDs, 999 and 1010.
+1188511880-1188511890 still has one invalid ID, 1188511885.
+222220-222224 still has one invalid ID, 222222.
+1698522-1698528 still contains no invalid IDs.
+446443-446449 still has one invalid ID, 446446.
+38593856-38593862 still has one invalid ID, 38593859.
+565653-565659 now has one invalid ID, 565656.
+824824821-824824827 now has one invalid ID, 824824824.
+2121212118-2121212124 now has one invalid ID, 2121212121.
+Adding up all the invalid IDs in this example produces 4174379265.
+
+What do you get if you add up all of the invalid IDs using these new rules?
+"""
+
+def find_repeated_sequence_duplicates(low:str, high:str):
+    low_int = int(low)
+    high_int = int(high)
+
+    low_len = len(low)
+    high_len = len(high)
+
+    out = []
+
+    for k in range(1, high_len//2 + 1):
+        low_n_mul = low_len // k
+        high_n_mul = high_len // k
+
+        for i in range(max(low_n_mul,1), high_n_mul +1):
+            start_int = int(low[:k])
+            new_n = int(str(start_int) * i)
+
+            if(new_n < 10):
+                #the digit has to be repeated twice, no n under 10 can be that
+                continue
+
+            if(i == 1):
+                new_n_str = str(new_n)
+                if new_n == int(new_n_str[0] * len(new_n_str)):
+                    if low_int <= new_n <= high_int and new_n not in out:
+                        out.append(new_n)
+
+            else:
+                while new_n <= high_int:
+                    if(low_int <= new_n and new_n not in out):
+                        out.append(new_n)
+                    start_int += 1
+                    new_n = int(str(start_int) * i)
+
+                start_int = int(low[:k])
+                new_n = int(str(start_int) * i)
+                while low_int <= new_n:
+                    if(new_n <= high_int and new_n not in out):
+                        out.append(new_n)
+                    start_int -= 1
+                    new_n = int(str(start_int) * i)
+
+    return out
+
+def find_repeated_sequence_duplicates_sum(data_list):
+    all_dupes = []
+    s = 0
+    for d in data_list:
+        low, high = d.split("-")
+        dupes = find_repeated_sequence_duplicates(low, high)
+        all_dupes.append(dupes)
+        s += sum(dupes)
+    return all_dupes, s
+
+
+def dupe_test(n_str):
+    is_dupe_list = []
+    for k in range(1,len(n_str) // 2 + 1):
+        template = n_str[:k]
+        is_dupe = True
+        for i in range(k, len(n_str), k):
+            if(n_str[i:i+k] != template):
+                is_dupe = False
+        is_dupe_list.append(is_dupe)
+    assert any(is_dupe_list), f"{n_str} is not a dupe"
+
+
+    #if(len(n_str) % 2 == 1):
+    #    assert n_str == n_str[0] * len(n_str), f"{n_str} is not a dupe"
+    #else:
+    #    assert n_str[:len(n_str) // 2] == n_str[len(n_str) // 2:], f"{n_str} is not a dupe"
+
+def main_2():
+    with open("day_2.txt") as f:
+        data = f.read().strip().split(",")
+
+    dupes, s = find_repeated_sequence_duplicates_sum(data)
+
+    flat_dupes = [x for xs in dupes for x in xs]
+    flat_dupes_true = [dupe_test(str(x)) for x in flat_dupes]
+
+
+    range_dupes = [[r, d] for r, d in zip(data, dupes)]
+
+    print(dupes)
+    print(s) #34421651195
+
+
+def main_2_test():
+    test_data = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    test_data_list = test_data.strip().split(",")
+    expected_dupes = set([11, 22,99,111,999, 1010, 1188511885, 222222, 446446, 38593859, 565656, 824824824, 2121212121])
+
+    dupes, s = find_repeated_sequence_duplicates_sum(test_data_list)
+
+    flat_dupes = [x for xs in dupes for x in xs]
+
+    dupes_set = set(flat_dupes)
+    if(0 in dupes_set):
+        dupes_set.remove(0)
+
+    assert len(dupes_set.difference(expected_dupes)) == 0
+
+    assert s == 4174379265
+
+def main_2_test_2():
+    test_data_list = ['5140337-5233474']
+    dupes, s = find_repeated_sequence_duplicates_sum(test_data_list)
+
+    expected_dupes = set(
+        [])
+
+
+
+if __name__ == '__main__':
+    #main_1_test()
+    #main_1()
+
+    main_2_test()
+    main_2()
+
 
 
 
