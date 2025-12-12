2284792a1360fa42d913e7ff227959b202cbf1cc
EsbernTK <esberntk@gmail.com>
2025-12-13 00:16:36 +0100

    finished day 4

--- day_4.py
+++ day_4.py
@@ -125,7 +125,207 @@ def main_1():
 
     print(s)
 
-if __name__ == '__main__':
-    main_test_1()
-    main_1()
+"""
+--- Part Two ---
+Now, the Elves just need help accessing as much of the paper as they can.
 
+Once a roll of paper can be accessed by a forklift, it can be removed. Once a roll of paper is removed, the forklifts might be able to access more rolls of paper, which they might also be able to remove. How many total rolls of paper could the Elves remove if they keep repeating this process?
+
+Starting with the same example as above, here is one way you could remove as many rolls of paper as possible, using highlighted @ to indicate that a roll of paper is about to be removed, and using x to indicate that a roll of paper was just removed:
+
+Initial state:
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+
+Remove 13 rolls of paper:
+..xx.xx@x.
+x@@.@.@.@@
+@@@@@.x.@@
+@.@@@@..@.
+x@.@@@@.@x
+.@@@@@@@.@
+.@.@.@.@@@
+x.@@@.@@@@
+.@@@@@@@@.
+x.x.@@@.x.
+
+Remove 12 rolls of paper:
+.......x..
+.@@.x.x.@x
+x@@@@...@@
+x.@@@@..x.
+.@.@@@@.x.
+.x@@@@@@.x
+.x.@.@.@@@
+..@@@.@@@@
+.x@@@@@@@.
+....@@@...
+
+Remove 7 rolls of paper:
+..........
+.x@.....x.
+.@@@@...xx
+..@@@@....
+.x.@@@@...
+..@@@@@@..
+...@.@.@@x
+..@@@.@@@@
+..x@@@@@@.
+....@@@...
+
+Remove 5 rolls of paper:
+..........
+..x.......
+.x@@@.....
+..@@@@....
+...@@@@...
+..x@@@@@..
+...@.@.@@.
+..x@@.@@@x
+...@@@@@@.
+....@@@...
+
+Remove 2 rolls of paper:
+..........
+..........
+..x@@.....
+..@@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@x.
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+...@@.....
+..x@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+...x@.....
+...@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+....x.....
+...@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+..........
+...x@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+Stop once no more rolls of paper are accessible by a forklift. In this example, a total of 43 rolls of paper can be removed.
+
+Start with your original diagram. How many rolls of paper in total can be removed by the Elves and their forklifts?
+"""
+
+
+def find_and_replace_free_slots(grid):
+
+    grid_out, s = find_free_slots(grid)
+    total_removed = s
+    while s > 0:
+        grid = grid - grid_out
+        assert np.all(np.abs(grid) == grid) #check for negatives
+        grid_out, s = find_free_slots(grid)
+        total_removed += s
+
+    return grid_out, total_removed
+
+
+def main_test_2():
+    test_data = """
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
+
+    expected_data = """
+..........
+..........
+..........
+...x@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@..."""
+
+
+    grid = create_grid(test_data)
+
+    expected_grid_out = create_grid(expected_data, "x")
+
+    grid_out, s = find_and_replace_free_slots(grid)
+
+    expected_grid_out = expected_grid_out.astype(int)
+    grid_out = grid_out.astype(int)
+
+    grid_out_diff = (expected_grid_out - grid_out).astype(int)
+
+    #assert np.allclose(grid_out, expected_grid_out)
+    assert s == 43
+
+
+
+def main_2():
+    with open("day_4.txt", "r") as f:
+        data = f.read()
+
+    grid = create_grid(data)
+    grid_out, s = find_and_replace_free_slots(grid)
+
+    print(s)
+
+
+if __name__ == '__main__':
+    #main_test_1()
+    #main_1()
+
+    main_test_2()
+    main_2()
